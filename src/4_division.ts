a33cee8a53a2bb49e3772446b8c2bccf3637004d
Lambdius <JavaScriptizer101@yandex.ru>
2022-07-09 00:01:53 +0300

    global fix

--- src/4_division.ts
+++ src/4_division.ts
@@ -1,47 +1,78 @@
 'use strict'
 
-import { sub }  from './2_subtraction'
+
+
+import { add }  from './1_addition'
+import { isBigger, sub }  from './2_subtraction'
+import { mul }  from './3_multiplication'
+
+
+
+function nSlice( numA: string, numB: string ): Array<string>
+{
+    let result: string = "1",
+        temp: string = mul( numB, "2" )
+
+    while ( !isBigger( temp, numA ) ) 
+    {
+        result = mul( result, "2" )
+        numB = temp
+        temp = mul( numB, "2" )
+    }
+
+    return [ numB, result ]
+}
 
 function recSub( a: string, b: string ): Array<string>
 {
-    let intermediate: string = "", 
-        count: number = 0;
-    
-    while ( (intermediate = sub(a,b))[0] !== "-" ) {
+    let intermediate = "", 
+        count = 0
+    while ( ( intermediate = sub( a, b ) )[0] !== "-" ) {
         a = intermediate
         count++
     }
     return [ String(count), a ]
 }
 
-
-export function div ( a: string, b: string ): any
+export function div( numA: string, numB: string ): string 
 {
-    let [ intR, fractR ]: Array<string> = [ "", "" ],
-        [ intA, fractA = "" ]: Array<string> = a.split("."),
-        [ intB, fractB = "" ]: Array<string> = b.split("."),
-        remainder: string = "",
-        sign: string = Number(/^-/.test(b)) + Number(/^-/.test(a)) === 1 ? "-" : ""
+    if ( /^-?0+\.?(0+)?$/.test(numB) ) throw Error();
+    if ( /^-?0+\.?(0+)?$/.test(numA) ) return "0"
 
-    
+    const sign: string = Number( numA[0] === "-" ) + Number( numB[0] === "-" ) === 1 ? "-" : ""
+
+    numA = numA.replace(/^-/,"")
+    numB = numB.replace(/^-/,"")
+
+    let [ intR, fractR ] = [ "0", "" ],
+        [ intA, fractA = "" ] = numA.split("."),
+        [ intB, fractB = "" ] = numB.split("."),
+        remainder = ""
+
     if ( fractA.length < fractB.length ) fractA += "0".repeat( fractB.length - fractA.length )
     if ( fractB.length < fractA.length ) fractB += "0".repeat( fractA.length - fractB.length )
-    
-    a = intA + fractA
-    b = intB + fractB
 
-    ;[ intR, remainder ] = recSub(a,b)
+    numA = ( intA + fractA ).replace(/^0+/, "")
+    numB = ( intB + fractB ).replace(/^0+/, "")
 
-    if ( remainder === "0" ) return sign + intR
+    let [ numX, localRes ]: Array<string> = nSlice( numA, numB )
 
-    for ( let i: number = 0; i < 20; i++ )
-    {
-        let [ x, y ] = recSub( remainder + "0", b )
+    while ( !isBigger( numX, numA ) ) {
+        intR = add( intR, localRes )
+        numA = sub( numA, numX )
+        ;[ numX, localRes ] = nSlice( numA, numB )
+    }
+
+    remainder = numA
+
+    for ( let i = 0; i < 20; i++ ) {
+        let [ x, y ] = recSub( remainder + "0", numB )
+        if ( remainder === "0" ) return sign + intR + ( "." + fractR ).replace(/\.(0+)?$/,"")
         fractR += x
         remainder = y
     }
 
-    fractR = ( "." + fractR ).replace(/\.(0+)?$/,"")
+    fractR = ( "." + fractR ).replace(/\.?(0+)?$/,"")
 
     return sign + intR + fractR
 }
